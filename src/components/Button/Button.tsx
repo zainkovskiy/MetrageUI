39e84b02b5fa02d1a959d662218e8d05982921a8
Anton Zainkovskiy <Piton1808@gmail.com>
2023-07-16 16:59:09 +0200

    style not worked

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -4,13 +4,7 @@ import { styled } from "styled-components";
 export interface ButtonProps {
   label: string;
 }
-const ButtonStyle = styled.button`
-  padding: 0.5rem 1rem;
-  background: white;
-  border: 1px solid black;
-  outline: none;
-`
 const Button = (props: ButtonProps) => {
-  return <ButtonStyle>{props.label}</ButtonStyle>;
+  return <button>{props.label}</button>;
 };
 export default Button;
