09825148a66168d23d0a6d1c8502726e73a6d3d7
Anton Zainkovskiy <Piton1808@gmail.com>
2023-07-16 16:55:18 +0200

    style component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,10 +1,16 @@
 import React from "react";
+import { styled } from "styled-components";
 
 export interface ButtonProps {
   label: string;
 }
-
+const ButtonStyle = styled.button`
+  padding: 0.5rem 1rem;
+  background: white;
+  border: 1px solid black;
+  outline: none;
+`
 const Button = (props: ButtonProps) => {
-  return <button>{props.label}</button>;
+  return <ButtonStyle>{props.label}</ButtonStyle>;
 };
 export default Button;
